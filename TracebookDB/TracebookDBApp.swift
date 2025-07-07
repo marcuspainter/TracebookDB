a58785e4da4bdb2617157d47e326092566970ff7
Marcus Painter <marcus@marcuspainter.com>
2025-07-07 22:01:26 +0100

    Initial Commit

---
+++ TracebookDB/TracebookDBApp.swift
@@ -0,0 +1,17 @@
+//
+//  TracebookDBApp.swift
+//  TracebookDB
+//
+//  Created by Marcus Painter on 07/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct TracebookDBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TracebookDBUITests/TracebookDBUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  TracebookDBUITestsLaunchTests.swift
+//  TracebookDBUITests
+//
+//  Created by Marcus Painter on 07/07/2025.
+//
+
+import XCTest
+
+final class TracebookDBUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
